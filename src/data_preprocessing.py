d5f9f58cb192ecb64987284229391d1efbef7ef9
Srijan76-code <srijanpatel911@gmail.com>
2026-02-14 23:58:11 +0530

    1.Preprocessing is done

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -4,3 +4,19 @@ data_preprocessing.py
 Handles loading, cleaning, and transforming the raw Telco Customer Churn
 dataset into a format suitable for feature engineering and model training.
 """
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns   
+from pathlib import Path
+
+
+
+
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+data_path = BASE_DIR / "data" / "raw" / "telecom_customer_churn.csv"
+
+df = pd.read_csv(data_path)
+df.head()
